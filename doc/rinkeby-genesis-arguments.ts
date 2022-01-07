8d0824be5456d437646c168711d167d2c6c6999d
Gabriel Cartier <gcartier@samsao.co>
2022-01-07 17:19:08 -0500

    updated args

--- doc/rinkeby-genesis-arguments.ts
+++ doc/rinkeby-genesis-arguments.ts
@@ -1,7 +1,7 @@
 import { networkConfig } from '../helper-hardhat-config';
 
 module.exports = [
-  '0x81360eDEF3b9F3639fA60639729881Aba9Fe29B1',
+  '0xbf0879e16230c31A13aa6669cb68b62CC96a0BDA',
   networkConfig[4].unrevealedURI,
   networkConfig[4].mintPrice,
   networkConfig[4].openSeaProxyAddress,
