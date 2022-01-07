40f3279cc7c4c0f215a53ee308e24d1db1add008
Gabriel Cartier <gcartier@samsao.co>
2022-01-07 04:30:38 -0500

    updated config

--- deploy/00_deployGenesis.ts
+++ deploy/00_deployGenesis.ts
@@ -2,21 +2,15 @@ import { ethers } from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { networkConfig } from '../helper-hardhat-config';
-import { LinkToken } from '../typechain';
 
 const deployGenesis: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment,
 ) {
   const { deployments, getNamedAccounts, getChainId } = hre;
-  const { deploy, get } = deployments;
+  const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  const deployerSigner = await ethers.getSigner(deployer);
   const chainId = await getChainId();
 
-  const linkTokenAddress = networkConfig[chainId].linkToken;
-  const vrfCoordinatorAddress = networkConfig[chainId].vrfCoordinator;
-  const keyHash: string = networkConfig[chainId].keyHash;
-  const chainlinkFee: string = networkConfig[chainId].chainlinkFee;
   const mintPrice: string = networkConfig[chainId].mintPrice;
   const unrevealedURI: string = networkConfig[chainId].unrevealedURI;
   const openSeaProxyAddress: string =
@@ -24,19 +18,9 @@ const deployGenesis: DeployFunction = async function (
 
   // Test network deployment only
   if (chainId === '31337') {
-    const linkToken = await get('LinkToken');
-    const VRFCoordinatorMock = await get('VRFCoordinatorMock');
-    console.log(
-      `Deploying GenesisSupply on ${chainId} with link address ${linkToken.address} and coordinator address ${VRFCoordinatorMock.address}`,
-    );
+    console.log(`Deploying GenesisSupply on ${chainId}`);
     const genesisSupply = await deploy('GenesisSupply', {
       from: deployer,
-      args: [
-        VRFCoordinatorMock.address,
-        linkToken.address,
-        keyHash,
-        chainlinkFee,
-      ],
       log: true,
     });
     console.log(
@@ -61,26 +45,13 @@ const deployGenesis: DeployFunction = async function (
       name: 'Genesis',
       address: genesis.address,
     });
-    const LinkContract = await ethers.getContractFactory(
-      'LinkToken',
-      deployerSigner,
-    );
 
-    const link = (await LinkContract.attach(linkToken.address)) as LinkToken;
-    const receipt = await link.transfer(
-      genesisSupply.address,
-      '10000000000000000000000000',
-    );
-    await receipt.wait();
-    console.log(`Genesis supply (${genesisSupply.address}) funded with LINK`);
-    const balance = await link.balanceOf(genesisSupply.address);
-    console.log(`Genesis supply LINK balance: ${balance}`);
     // Live networks
   } else {
     console.log(`Live network deploying GenesisSupply on ${chainId}`);
     const genesisSupply = await deploy('GenesisSupply', {
       from: deployer,
-      args: [vrfCoordinatorAddress, linkTokenAddress, keyHash, chainlinkFee],
+      args: [],
       log: true,
     });
     console.log(`Live network deploying Genesis on ${chainId}`);
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,12 +1,8 @@
 export interface networkConfigItem {
   name: string;
-  chainlinkFee: string;
   mintPrice: string;
-  keyHash: string;
   unrevealedURI: string;
   openSeaProxyAddress: string;
-  linkToken?: string;
-  vrfCoordinator?: string;
 }
 
 export interface networkConfigInfo {
@@ -16,53 +12,33 @@ export interface networkConfigInfo {
 export const networkConfig: networkConfigInfo = {
   default: {
     name: 'hardhat',
-    chainlinkFee: '100000000000000000',
-    keyHash:
-      '0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4',
     mintPrice: '77000000000000000',
-    unrevealedURI: 'ipfs://QmeZ9zGtFszp9q3fcK7MSoussjm4nBqR8xbQ1z8oDcGDYa',
+    unrevealedURI: 'ipfs://QmeZ9zGtFszp9q3fcK7MSoussjm4nBqR8xbQ1z8oDcGDYa/',
     openSeaProxyAddress: '0xf57b2c51ded3a29e6891aba85459d600256cf317',
   },
   31337: {
     name: 'localhost',
-    chainlinkFee: '100000000000000000',
-    keyHash:
-      '0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4',
     mintPrice: '77000000000000000',
-    unrevealedURI: 'ipfs://QmeZ9zGtFszp9q3fcK7MSoussjm4nBqR8xbQ1z8oDcGDYa',
+    unrevealedURI: 'ipfs://QmeZ9zGtFszp9q3fcK7MSoussjm4nBqR8xbQ1z8oDcGDYa/',
     openSeaProxyAddress: '0xf57b2c51ded3a29e6891aba85459d600256cf317',
   },
   42: {
     name: 'kovan',
-    linkToken: '0xa36085F69e2889c224210F603D836748e7dC0088',
-    keyHash:
-      '0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4',
-    vrfCoordinator: '0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9',
-    chainlinkFee: '2000000000000000000',
     mintPrice: '77000000000000000',
-    unrevealedURI: 'ipfs://QmeZ9zGtFszp9q3fcK7MSoussjm4nBqR8xbQ1z8oDcGDYa',
+    unrevealedURI: 'ipfs://QmeZ9zGtFszp9q3fcK7MSoussjm4nBqR8xbQ1z8oDcGDYa/',
     openSeaProxyAddress: '0xf57b2c51ded3a29e6891aba85459d600256cf317',
   },
   1: {
+    // TODO Update with valid URI
     name: 'mainnet',
-    linkToken: '0x514910771af9ca656af840dff83e8264ecf986ca',
-    keyHash:
-      '0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445',
-    vrfCoordinator: '0xf0d54349aDdcf704F77AE15b96510dEA15cb7952',
-    chainlinkFee: '2000000000000000000',
     mintPrice: '77000000000000000',
-    unrevealedURI: 'ipfs://Qmf28aAuBptYHtVE9pDoU9sPtFV4U898zTsBuMjBxFXYKQ',
+    unrevealedURI: 'ipfs://Qmf28aAuBptYHtVE9pDoU9sPtFV4U898zTsBuMjBxFXYKQ/',
     openSeaProxyAddress: '0xa5409ec958c83c3f309868babaca7c86dcb077c1',
   },
   4: {
     name: 'rinkeby',
-    linkToken: '0x01BE23585060835E02B77ef475b0Cc51aA1e0709',
-    keyHash:
-      '0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311',
-    vrfCoordinator: '0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B',
-    chainlinkFee: '100000000000000000',
     mintPrice: '77000000000000000',
-    unrevealedURI: 'ipfs://QmeZ9zGtFszp9q3fcK7MSoussjm4nBqR8xbQ1z8oDcGDYa',
+    unrevealedURI: 'ipfs://QmeZ9zGtFszp9q3fcK7MSoussjm4nBqR8xbQ1z8oDcGDYa/',
     openSeaProxyAddress: '0xf57b2c51ded3a29e6891aba85459d600256cf317',
   },
 };
